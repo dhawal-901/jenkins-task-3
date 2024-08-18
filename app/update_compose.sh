4bb99a178e33f9eefb61500f1e1981ea9b508d59
Dhawal Vijayvargiya <dhawal@gkmit.co>
2024-08-18 11:46:26 +0530

    Created script to update compose file with the latest image.

---
+++ app/update_compose.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+COMPOSE_FILE="compose.yml"
+old_image=$(awk '/dhawal901\/jenkins-task-3/ {print}' "$COMPOSE_FILE")
+new_version="${env.BUILD_NUMBER}"
+new_image=$(echo "$old_image" | sed -E "s/:[0-9]+/:$new_version/")
+sed -i.bak "s|$old_image|$new_image|" "$COMPOSE_FILE"
+
+echo "Updated $COMPOSE_FILE with new image version: $new_version"
