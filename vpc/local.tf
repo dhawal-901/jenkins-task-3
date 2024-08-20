e212c07e7db5512c965e9d2ae8a0986cd000aaf7
Dhawal Vijayvargiya <dhawal@gkmit.co>
2024-08-20 20:35:28 +0530

    Updated the certificate arn.

--- vpc/local.tf
+++ vpc/local.tf
@@ -14,7 +14,8 @@ locals {
 
       target_group_1_name = "lg-target-1-dev"
       target_group_2_name = "lg-target-2-dev"
-      certificate_1_arn   = "arn:aws:acm:ap-south-1:905418378622:certificate/50d376a0-bc69-4290-8951-7085df950adc"
+      certificate_1_arn   = "arn:aws:acm:ap-south-1:905418378622:certificate/eac00c02-751c-415f-95ba-ba4a3d71337d"
+      # certificate_1_arn   = "arn:aws:acm:ap-south-1:905418378622:certificate/50d376a0-bc69-4290-8951-7085df950adc"
       # certificate_2_arn   = "arn:aws:acm:ap-south-1:905418378622:certificate/be474a6e-e297-49cf-a678-77bb11273be4"
 
       public_instance_ami   = "ami-0ad21ae1d0696ad58"
